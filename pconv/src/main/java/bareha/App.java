f8cb6cd162637d263624950ad347fc3941982b09
Bareha Mukhtar <barehamukhtar@gmail.com>
2025-01-17 01:02:00 +0530

    syntax fixes, token generation

--- pconv/src/main/java/bareha/App.java
+++ pconv/src/main/java/bareha/App.java
@@ -1,24 +1,30 @@
 package bareha;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws URISyntaxException, IOException, InterruptedException
     {
         HttpClient client = HttpClient.newHttpClient(); // creating client
         String client_id = "800a13ce72b04d4eb5636c5b3af6beb6";
         String client_secret = "dc3e86d9fef0440aa4a2bb419fe316cb";
-        String body = StringBuilder("grant_type=client_credentials&client_id=%s&client_secret=%s", client_id, client_secret);
+        String body = String.format("grant_type=client_credentials&client_id=%s&client_secret=%s", client_id, client_secret);
 
         // this is to send a request
         HttpRequest request = HttpRequest.newBuilder()
             .uri(new URI("https://accounts.spotify.com/api/token"))
-            .header("Content-Type: application/x-www-form-urlencoded")
+            .header("Content-Type", "application/x-www-form-urlencoded")
             .POST(HttpRequest.BodyPublishers.ofString(body))
             .build();
 
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()); // this will get response
+
+        System.out.println("response code: " + response.statusCode());
+        System.out.println("response body: " + response.body());
     }
 }
