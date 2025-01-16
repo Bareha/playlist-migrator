189beb1f963d8577eb18fdd43d08a399dc0efa94
Bareha Mukhtar <barehamukhtar@gmail.com>
2025-01-16 23:21:28 +0530

    added imports

--- pconv/src/main/java/bareha/App.java
+++ pconv/src/main/java/bareha/App.java
@@ -1,13 +1,9 @@
 package bareha;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        private static final string = "";
     }
 }
